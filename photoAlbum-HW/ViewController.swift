ce040b4eb171162c60eda0e1596b11ed8343eac8
Roman Nazarov <ste7ww@gmail.com>
2022-07-08 09:50:47 +0500

    Initial Commit

---
+++ photoAlbum-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photoAlbum-HW
+//
+//  Created by Роман Назаров on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
